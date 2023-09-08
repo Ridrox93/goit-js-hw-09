68e020dc51db580b0cafdd444b8e4671f05fc9ba
Ridrox93 <popovandrii@yahoo.com>
2023-09-08 18:14:24 +0200

    6-add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-// world
+const buttonStartEl = document.querySelector('[data-start]');
+const buttonStoptEl = document.querySelector('[data-start]');
+const bodyEL = document.querySelector("body");
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+
+let colorChanger = null;
+
+buttonStartEl.addEventListener("click", ()=> {colorChanger = setInterval(() => {
+    buttonStartEl.disabled = true;
+    buttonStoptEl.disabled = false;
+    bodyEL.style.backgroundColor = getRandomHexColor();
+    }, 2000);
+  });
+
+buttonStoptEl.addEventListener("click", () => {
+    buttonStartEl.disabled = false;
+    buttonStoptEl.disabled = true;
+    clearInterval(colorChanger);
+    });
+
+
+
